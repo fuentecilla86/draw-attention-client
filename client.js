8a5d9578b2049570c996e7d30014b6167978810e
Adrian <fuentecilla86>
2018-03-25 17:57:45 +0200

    add LED behavior

--- client.js
+++ client.js
@@ -1,6 +1,9 @@
 //client.js
+var Gpio = require('onoff').Gpio;
+var LED = new Gpio(14, 'out');
 var io = require('socket.io-client');
 var socket = io.connect('http://localhost:3000', {reconnect: true});
+
 
 // Add a connect listener
 socket.on('connect', function (socket) {
@@ -9,6 +12,11 @@ socket.on('connect', function (socket) {
 
 socket.on('switchUpdate', (state) => {
   console.log('LED ' + state);
+  if (state) {
+    LED.writeSync(1);
+  } else {
+    LED.writeSync(0);
+  }
 });
 
 socket.emit('send-nickname', 'raspberrypi');
