7de45af3045eeb17b54099d9f87197c70c5601a6
Adrian <fuentecilla86>
2018-04-14 18:29:29 +0200

    fixes

--- client.js
+++ client.js
@@ -1,8 +1,8 @@
 //client.js
-var Gpio = require('onoff').Gpio;
-var LED = new Gpio(14, 'out');
+// var Gpio = require('onoff').Gpio;
+// var LED = new Gpio(14, 'out');
 var io = require('socket.io-client');
-var socket = io.connect('http://localhost:3000', {reconnect: true});
+var socket = io.connect('http://localhost:3001', {reconnect: true});
 
 
 // Add a connect listener
@@ -10,13 +10,13 @@ socket.on('connect', function (socket) {
     console.log('Connected!');
 });
 
-socket.on('switchUpdate', (state) => {
+socket.on('switch-update', (state) => {
   console.log('LED ' + state);
-  if (state) {
-    LED.writeSync(1);
-  } else {
-    LED.writeSync(0);
-  }
+  // if (state) {
+  //   LED.writeSync(1);
+  // } else {
+  //   LED.writeSync(0);
+  // }
 });
 
 socket.emit('send-nickname', 'raspberrypi');
