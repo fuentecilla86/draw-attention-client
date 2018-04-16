a2532e513cf66ce5777bc63f6683daab3b22151b
Adrian <fuentecilla86>
2018-04-16 22:14:47 +0200

    fix reconnection

--- client.js
+++ client.js
@@ -1,6 +1,7 @@
 //client.js
 // var Gpio = require('onoff').Gpio;
 // var LED = new Gpio(14, 'out');
+var sleep = require('sleep');
 var io = require('socket.io-client');
 var socket = io.connect('http://localhost:3001', {reconnect: true});
 
@@ -8,15 +9,19 @@ var socket = io.connect('http://localhost:3001', {reconnect: true});
 // Add a connect listener
 socket.on('connect', function (socket) {
     console.log('Connected!');
+    this.emit('send-nickname', 'raspberrypi');
 });
 
 socket.on('switch-update', (state) => {
   console.log('LED ' + state);
-  // if (state) {
-  //   LED.writeSync(1);
-  // } else {
-  //   LED.writeSync(0);
-  // }
+  if (state) {
+      //LED.writeSync(1);
+      console.log('Encendido');
+
+  } else {
+    //LED.writeSync(0)
+    console.log('Led apagado')
+  }
 });
 
-socket.emit('send-nickname', 'raspberrypi');
+// socket.emit('send-nickname', 'raspberrypi');
